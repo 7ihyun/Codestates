7b1dee886cb436c2b939a5d536a970eda4ed4bc9
Kim-Jihyun1 <jh129047@gmail.com>
2022-09-23 16:27:57 +0900

    Tree & Graph & Binary Search Tree

---
+++ src/Datastructure/Ex06.java
@@ -0,0 +1,71 @@
+package Datastructure;
+
+import java.util.*;
+
+public class Ex06 {
+    private String value;
+    private ArrayList<Ex06> children;
+
+    public Ex06() {	//전달인자가 없을 경우의 생성자
+        this.value = null;
+        this.children = null;
+    }
+    public Ex06(String data) {	//전달인자가 존재할 경우의 생성자
+        this.value = data;
+        this.children = null;
+    }
+
+    public void setValue(String data) {
+        this.value = data;
+    }
+
+    public String getValue() {      //현재 노드의 데이터를 반환
+        return value;
+    }
+    public void addChildNode(Ex06 node) {
+        if(children == null) children = new ArrayList<>();
+        children.add(node);
+    }
+    public void removeChildNode(Ex06 node) {
+        String data = node.getValue();
+
+        if(children != null) {
+            for(int i = 0; i < children.size(); i++) {
+                if(children.get(i).getValue().equals(data)) {
+                    children.remove(i);
+                    return;
+                }
+                if(children.get(i).children != null) children.get(i).removeChildNode(node);
+            }
+        }
+    }
+
+    public ArrayList<Ex06> getChildrenNode() {
+        return children;
+    }
+
+    public boolean contains(String data) {      //재귀를 사용하여 값을 검색합니다.
+        if(value.equals(data)) return true;
+
+        boolean check;
+
+        if(children != null) {
+            for(int i = 0; i < children.size(); i++) {
+                check = children.get(i).contains(data, false);
+                if(check) return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean contains(String data, boolean check) {      //재귀를 사용하여 값을 검색합니다.
+        if(value.equals(data)) return true;
+
+        if(children != null) {
+            for(int i = 0; i < children.size(); i++) {
+                check = children.get(i).contains(data, check);
+            }
+        }
+        return check;
+    }
+}
